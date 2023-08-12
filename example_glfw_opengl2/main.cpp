da73e79a99095447ed72a5b9ab89e23058c2f451
Robert Shepherd <rshepherdcpp@gmail.com>
2023-08-12 12:58:08 +0100

    complete the first aimbot tab

--- example_glfw_opengl2/main.cpp
+++ example_glfw_opengl2/main.cpp
@@ -15,10 +15,8 @@
 #define GL_SILENCE_DEPRECATION
 #endif
 #include <GLFW/glfw3.h>
+#include <fstream>
 
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
@@ -28,109 +26,244 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-// Main code
+bool AimbotIsOn = true;
+
+const char* types[] = { "Normal" };
+static int types_item_current_idx = 0;
+
+const char* filter[] = { "Visible Check" };
+static int filter_item_current_idx = 0;
+
+const char* priority[] = { "Distance" };
+static int priority_item_current_idx = 0;
+
+bool AimRequired = true;
+
+static float min_amin_amount = 0.5f;
+
+bool stickToTarget = true;
+bool ignoreDowned = true;
+bool FOVcheck = true;
+
+int FOVradius = 320;
+int smoothAmount = 70;
+int smoothAcceleration = 90;
+
+const char* target_bone[] = { "Helmet" };
+static int target_bone_item_current_idx = 0;
+
+bool bodyScan = true;
+bool boneBodyScanFirst = false;
+
+bool autoShoot = false;
+bool triggerBot = false;
+
+auto save_to_file() -> void
+{
+    std::ofstream file("aimbot_menu.txt");
+    file << "aimbot:               " << AimbotIsOn << "\n";
+    file << "types:                " << types_item_current_idx << "\n";
+    file << "filter:               " << filter_item_current_idx << "\n";
+    file << "priority:             " << priority_item_current_idx << "\n";
+    file << "aim required:         " << AimRequired << "\n";
+    file << "min amin amount:      " << min_amin_amount << "\n";
+    file << "stick to target:      " << stickToTarget << "\n";
+    file << "ignore downed:        " << ignoreDowned << "\n";
+    file << "FOV check:            " << FOVcheck << "\n";
+    file << "FOV radius:           " << FOVradius << "\n";
+    file << "smooth amount:        " << smoothAmount << "\n";
+    file << "smooth acceleration:  " << smoothAcceleration << "\n";
+    file << "target_bone:          " << target_bone_item_current_idx << "\n";
+    file << "body scan:            " << bodyScan << "\n";
+    file << "bone body scan first: " << boneBodyScanFirst << "\n";
+    file << "auto shoot:           " << autoShoot << "\n";
+    file << "triggerbot:           " << triggerBot << "\n";
+}
+
 int main(int, char**)
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         return 1;
 
-    // Create window with graphics context
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL2 example", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    glfwSwapInterval(1);
 
-    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
-    // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL2_Init();
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
-
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    int num_screenshots_cleared = 0;
+    bool showAimbotMenu = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    // Main loop
     while (!glfwWindowShouldClose(window))
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
-
-        // Start the Dear ImGui frame
         ImGui_ImplOpenGL2_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        // This is where we begin the Aimbot menu.
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            ImGui::Begin("PUNCHWARE MW2 CHAIR", &showAimbotMenu);
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
+            if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
+            {
+                if (ImGui::BeginTabItem("Aimbot"))
+                {
+                    ImGui::Checkbox("Aimbot", &AimbotIsOn);
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+                    // the dropdown box for Type
+                    const char* type_combo_preview_value = types[types_item_current_idx];
+                    if (ImGui::BeginCombo("Type", type_combo_preview_value))
+                    {
+                        for (int n = 0; n < IM_ARRAYSIZE(types); n++)
+                        {
+                            const bool is_selected = (types_item_current_idx == n);
+                            if (ImGui::Selectable(types[n], is_selected))
+                                types_item_current_idx = n;
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+                            if (is_selected)
+                                ImGui::SetItemDefaultFocus();
+                        }
+                        ImGui::EndCombo();
+                    }
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+                    const char* filter_combo_preview_value = filter[filter_item_current_idx];
+                    if (ImGui::BeginCombo("Filter", filter_combo_preview_value))
+                    {
+                        for (int n = 0; n < IM_ARRAYSIZE(filter); n++)
+                        {
+                            const bool is_selected = (filter_item_current_idx == n);
+                            if (ImGui::Selectable(filter[n], is_selected))
+                                filter_item_current_idx = n;
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                            if (is_selected)
+                                ImGui::SetItemDefaultFocus();
+                        }
+                        ImGui::EndCombo();
+                    }
+
+                    const char* priority_combo_preview_value = priority[priority_item_current_idx];
+                    if (ImGui::BeginCombo("Priority", priority_combo_preview_value))
+                    {
+                        for (int n = 0; n < IM_ARRAYSIZE(priority); n++)
+                        {
+                            const bool is_selected = (priority_item_current_idx == n);
+                            if (ImGui::Selectable(priority[n], is_selected))
+                                priority_item_current_idx = n;
+
+                            if (is_selected)
+                                ImGui::SetItemDefaultFocus();
+                        }
+                        ImGui::EndCombo();
+                    }
+
+                    ImGui::Checkbox("Aim Required", &AimRequired);
+
+                    ImGui::SliderFloat("Min Amin Amount", &min_amin_amount, 0.0f, 1.0f, "%.3f");
+
+                    ImGui::Checkbox("Stick To Target", &stickToTarget);
+                    ImGui::Checkbox("Ignore Downed", &ignoreDowned);
+                    ImGui::Checkbox("FOV Check", &FOVcheck);
+
+                    ImGui::DragInt("FOV radius", &FOVradius, 0.5f, 0, 360, "%d");
+                    ImGui::DragInt("Smooth Amount", &smoothAmount, 0.5f, 0, 100, "%d");
+                    ImGui::DragInt("Smooth Acceleration", &smoothAcceleration, 0.5f, 0, 100, "%d");
+
+                    ImGui::Separator();
+
+                    const char* target_bone_combo_preview_value = target_bone[target_bone_item_current_idx];
+                    if (ImGui::BeginCombo("Target Bone", target_bone_combo_preview_value))
+                    {
+                        for (int n = 0; n < IM_ARRAYSIZE(target_bone); n++)
+                        {
+                            const bool is_selected = (target_bone_item_current_idx == n);
+                            if (ImGui::Selectable(target_bone[n], is_selected))
+                                target_bone_item_current_idx = n;
+
+                            if (is_selected)
+                                ImGui::SetItemDefaultFocus();
+                        }
+                        ImGui::EndCombo();
+                    }
+
+                    ImGui::Checkbox("Body scan", &bodyScan);
+                    ImGui::Checkbox("Bone body scan first", &boneBodyScanFirst);
+
+                    ImGui::Separator();
+
+                    ImGui::Checkbox("auto shoot", &autoShoot);
+                    ImGui::Checkbox("triggerbot", &triggerBot);
+
+                    ImGui::Separator();
+
+                    ImGui::Text("Screenshots Cleared: %d", num_screenshots_cleared);
+
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("ESP"))
+                {
+                    ImGui::Text("This is the Broccoli tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("Chams"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+
+                if (ImGui::BeginTabItem("Visuals"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+
+                if (ImGui::BeginTabItem("Removals"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+
+                if (ImGui::BeginTabItem("Misc"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+
+                if (ImGui::BeginTabItem("Settings"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+
+                if (ImGui::BeginTabItem("Configs"))
+                {
+                    ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                    ImGui::EndTabItem();
+                }
+                ImGui::EndTabBar();
+            }
             ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // Rendering
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
@@ -138,19 +271,12 @@ int main(int, char**)
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
-        // you may need to backup/reset/restore other state, e.g. for current shader using the commented lines below.
-        //GLint last_program;
-        //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-        //glUseProgram(0);
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        //glUseProgram(last_program);
 
         glfwMakeContextCurrent(window);
         glfwSwapBuffers(window);
     }
 
-    // Cleanup
     ImGui_ImplOpenGL2_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
