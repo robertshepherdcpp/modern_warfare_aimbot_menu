f589ec50ba4e6616693acb6e2e151b4883111a3c
Robert Shepherd <rshepherdcpp@gmail.com>
2023-08-13 10:29:46 +0100

    update the name on the window.

--- example_glfw_opengl2/main.cpp
+++ example_glfw_opengl2/main.cpp
@@ -824,13 +824,15 @@ int main(int, char**)
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         ImGui::Render();
+        
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        
 
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
